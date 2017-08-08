e970a3fe339a17b6508ba80e56825d6315ef1653
renuka <renutarse1@gmail.com>
2017-08-08 12:32:07 +0530

    1st

---
+++ ClassOne/src/com/scp/class1/ClassOne2.java
@@ -0,0 +1,18 @@
+package com.scp.class1;
+
+public class ClassOne2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassTwo.methodOfClassTwo();
+
+	}
+
+}
+class ClassTwo{
+static void methodOfClassTwo()
+{
+	System.out.println("from class two");
+}
+
+}
